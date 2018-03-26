cf8b9929524e267e735e240796c808f5dcd76808
hrishidubbewar <31519501+hrishidubbewar@users.noreply.github.com>
2018-03-26 20:32:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,54 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    Title : 'Article one | Hrishikesh Dubewar',
+    heading : 'Article one',
+    date : '15 march',
+    content: `<p>This is my first article.This is my first article.This is my first article.This is my first article.
+    This is my first article.This is my first article.This is my first article.This is my first article.
+    This is my first article.This is my first article.This is my first article.This is my first article.
+    This is my first article.This is my first article.This is my first article.This is my first article.</p>`
+};
+
+function createTemplate (data) {
+    var title = data.Title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    
+    var htmlTemplate = `
+        <html>
+            <head>
+                <title>
+                    ${Title}
+                </title>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <link href="/ui/style.css" rel="stylesheet" />
+            </head>
+            <body>
+                <div class="container">
+                    <div><href="/">Home</a></div>
+                    <hr/>
+                    <h3>
+                        ${heading}
+                    </h3>
+                    <div>
+                        ${date}
+                    </div>
+                    <div>
+                        ${content}
+                    </div>
+                </div>
+            </body>
+        </html>
+
+    `;
+    return htmlTemplate;
+}
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
