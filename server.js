c7656ed21094ece5c4e853815abb943764dfe973
hrishidubbewar <31519501+hrishidubbewar@users.noreply.github.com>
2018-03-26 20:40:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.get('/article-one', function (req, res) {
   res.send(createTemplate(articleOne));
 });
 
-app.get('/article-one', function (req, res) {
+app.get('/article-two', function (req, res) {
   res.send(createTemplate(articleTwo));
 });
 
